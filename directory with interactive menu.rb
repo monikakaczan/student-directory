2264ea44ccf6a3f36ca6dda95ef461b349526cf0
monikakaczan <kaczanowskamonika@gmail.com>
2019-05-22 21:46:46 +0100

    load students method added

--- directory with interactive menu.rb
+++ directory with interactive menu.rb
@@ -22,7 +22,8 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
-  puts "9. Exit"
+  puts "4. Load the list from students.csv"
+  puts "9. Exit"   
 end
 
 
@@ -68,6 +69,15 @@ def save_students
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
     file.puts csv_line
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
